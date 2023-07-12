e7b0223c05bc7f7d3dd241f80db12a2a1d931467
saurabhjadhav <saurabhthesuperhero@gmail.com>
2023-07-13 00:53:27 +0530

    inprocess UI

--- app/src/main/java/com/simplifymindfulness/selfbetter/screens/HabitTrackerScreen.kt
+++ app/src/main/java/com/simplifymindfulness/selfbetter/screens/HabitTrackerScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -26,11 +27,13 @@ import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -43,6 +46,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -50,6 +54,12 @@ import com.simplifymindfulness.selfbetter.Screen
 import com.simplifymindfulness.selfbetter.room.HabitStatus
 import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalDate
+import org.threeten.bp.ZoneId
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.TextStyle
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun HabitTrackerApp(viewModel: HabitViewModel, navController: NavController) {
@@ -58,6 +68,8 @@ fun HabitTrackerApp(viewModel: HabitViewModel, navController: NavController) {
     Column(modifier = Modifier.fillMaxSize()) {
         CalendarView(selectedDate = selectedDate)
         TaskListView(viewModel, selectedDate)
+//        ColorShadesView(MaterialTheme.colorScheme)
+
         Spacer(modifier = Modifier.weight(1f)) // This pushes the remaining content to the top
 // Pass the current date here
         Button(
@@ -72,6 +84,69 @@ fun HabitTrackerApp(viewModel: HabitViewModel, navController: NavController) {
 
     }
 }
+
+@Composable
+fun ColorShadesView(colorScheme: ColorScheme) {
+    val colorProperties = mapOf(
+        "Primary" to colorScheme.primary,
+        "OnPrimary" to colorScheme.onPrimary,
+        "PrimaryContainer" to colorScheme.primaryContainer,
+        "OnPrimaryContainer" to colorScheme.onPrimaryContainer,
+        "InversePrimary" to colorScheme.inversePrimary,
+        "Secondary" to colorScheme.secondary,
+        "OnSecondary" to colorScheme.onSecondary,
+        "SecondaryContainer" to colorScheme.secondaryContainer,
+        "OnSecondaryContainer" to colorScheme.onSecondaryContainer,
+        "Tertiary" to colorScheme.tertiary,
+        "OnTertiary" to colorScheme.onTertiary,
+        "TertiaryContainer" to colorScheme.tertiaryContainer,
+        "OnTertiaryContainer" to colorScheme.onTertiaryContainer,
+        "Background" to colorScheme.background,
+        "OnBackground" to colorScheme.onBackground,
+        "Surface" to colorScheme.surface,
+        "OnSurface" to colorScheme.onSurface,
+        "SurfaceVariant" to colorScheme.surfaceVariant,
+        "OnSurfaceVariant" to colorScheme.onSurfaceVariant,
+        "SurfaceTint" to colorScheme.surfaceTint,
+        "InverseSurface" to colorScheme.inverseSurface,
+        "InverseOnSurface" to colorScheme.inverseOnSurface,
+        "Error" to colorScheme.error,
+        "OnError" to colorScheme.onError,
+        "ErrorContainer" to colorScheme.errorContainer,
+        "OnErrorContainer" to colorScheme.onErrorContainer,
+        "Outline" to colorScheme.outline,
+        "OutlineVariant" to colorScheme.outlineVariant,
+        "Scrim" to colorScheme.scrim
+    )
+
+    LazyColumn {
+        items(colorProperties.entries.toList()) { (propertyName, colorValue) ->
+            colorValue?.let { color ->
+                ColorShadeItem(propertyName, color)
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun ColorShadeItem(name: String, color: Color) {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color)
+    ) {
+        val contentColor = contentColorFor(color)
+        Text(
+            text = name,
+            color = contentColor,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -91,12 +166,30 @@ fun CalendarView(selectedDate: MutableState<LocalDate>) {
     Column(modifier = Modifier.fillMaxWidth()) {
         val displayText = when {
             selectedDate.value == today -> "Today"
-            else -> selectedDate.value.toString()
+            selectedDate.value == today.plusDays(1) -> "Tomorrow"
+            else -> {
+                val dateFormat = if (selectedDate.value.year == today.year) {
+                    DateTimeFormatter.ofPattern("d MMM", Locale.getDefault())
+                } else {
+                    DateTimeFormatter.ofPattern("d MMM yyyy", Locale.getDefault())
+                }
+                selectedDate.value.format(dateFormat)
+            }
         }
+
         Text(
             text = displayText,
-            style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.padding(16.dp)
+            style = MaterialTheme.typography.headlineLarge.copy( fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 8.dp)
+        )
+
+        val weekName = selectedDate.value.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault())
+        Text(
+            text = weekName,
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Light),
+            modifier = Modifier.padding(start = 20.dp, bottom = 16.dp)
         )
 
         Row(
@@ -131,7 +224,7 @@ fun CalendarView(selectedDate: MutableState<LocalDate>) {
                             .clip(CircleShape)
                             .clickable(
                             ) { selectedDate.value = date }
-                            .background(if (date == selectedDate.value) MaterialTheme.colorScheme.onTertiary else Color.Transparent),
+                            .background(if (date == selectedDate.value) MaterialTheme.colorScheme.primaryContainer else Color.Transparent),
 
                         contentAlignment = Alignment.Center
                     ) {
@@ -187,10 +280,13 @@ fun TaskListView(
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp)
-                    .background(if (isTicked) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.surface),
+                    .padding(8.dp),
                 elevation = CardDefaults.cardElevation(
                     defaultElevation = 8.dp
+                )
+            ,
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
                 )
             ) {
                 Row(
@@ -211,15 +307,17 @@ fun TaskListView(
 
                     }) {
                         Icon(
-                            imageVector = if (isTicked) Icons.Default.CheckCircle else Icons.Default.Check,
+                            imageVector = if (isTicked) Icons.Default.CheckCircle else Icons.Default.CheckCircle,
                             contentDescription = if (isTicked) "Checked" else "Unchecked",
-                            tint = if (isTicked) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.onPrimary
+                            tint = if (isTicked) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.onSecondaryContainer
                         )
                     }
                     Spacer(modifier = Modifier.width(16.dp))
                     Text(
                         text = habit.name,  // Display the habit name
-                        style = MaterialTheme.typography.bodySmall
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onSecondary,
+
                     )
                 }
             }
